08a727336d4f9fd98aad793ffdfe464600b31068
FlavioMoreiraTec <flavio.moreira@outloook.com>
2016-04-16 13:18:37 -0300

    Coverter Links em Linha de Texto
    Link video youtube
    Link de Imagens

---
+++ convert_link.js
@@ -0,0 +1,24 @@
+$('.MyDiv').html(function(i, html) {
+	return html.
+		replace(/&/g,
+		'?'
+	).
+		replace(/(?:https:\/\/)?(?:http:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?([^\ ]+)/g,
+		'<p><object data="http://www.youtube.com/embed/$1"></object></p>'
+	).
+		replace(/(?:http:\/\/)?(?:www\.)?(?:vimeo\.com)\/([^\ ]+)/g,
+		'<p><object data="//player.vimeo.com/video/$1"></object></p>'
+	);
+});
+$(".MyDiv").each(function() {
+    if(!$(this).find('img').length){
+		$(this).html($(this).html().
+				replace(/(http:\/\/\S+(\.png|\.jpg|\.gif|\.webp))/g,
+				'<img src="$1" />'
+			).
+				replace(/(https:\/\/\S+(\.png|\.jpg|\.gif|\.webp))/g,
+				'<img src="$1" />'
+			)
+		);	 
+    }
+});
